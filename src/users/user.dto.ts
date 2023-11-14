8c860f479ca3cda5a029a23571c65fe196543bad
Marcin <wanago.marcin@gmail.com>
2023-11-14 01:28:47 +0100

    feat(Users): add the User model and the module

---
+++ src/users/user.dto.ts
@@ -0,0 +1,5 @@
+export class UserDto {
+  email: string;
+  name: string;
+  password: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,56 @@
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaError } from '../database/prisma-error.enum';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../database/prisma.service';
+import { UserDto } from './user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getByEmail(email: string) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return user;
+  }
+
+  async getById(id: number) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return user;
+  }
+
+  async create(user: UserDto) {
+    try {
+      return this.prismaService.user.create({
+        data: user,
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error?.code === PrismaError.UniqueConstraintFailed
+      ) {
+        throw new ConflictException('User with that email already exists');
+      }
+      throw error;
+    }
+  }
+}
