186304221348515adcd0ffe6e19b2462216cc19f
Marcin <wanago.marcin@gmail.com>
2023-11-21 04:36:39 +0100

    feat(Articles): create articles with authorId

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -26,9 +26,13 @@ export class ArticlesService {
     return article;
   }
 
-  create(article: CreateArticleDto) {
+  create(article: CreateArticleDto, authorId: number) {
     return this.prismaService.article.create({
-      data: article,
+      data: {
+        title: article.title,
+        text: article.text,
+        authorId,
+      },
     });
   }
 
