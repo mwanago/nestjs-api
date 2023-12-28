85bca690f4fa33e4ced96897619d46cb4fa1f3e2
Marcin <wanago.marcin@gmail.com>
2023-12-28 02:16:41 +0100

    test(Authentication): test the getAuthenticatedUser method

--- src/authentication/authentication.service.test.ts
+++ src/authentication/authentication.service.test.ts
@@ -2,21 +2,40 @@ import { AuthenticationService } from './authentication.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { Test } from '@nestjs/testing';
-import { UsersModule } from '../users/users.module';
-import { DatabaseModule } from '../database/database.module';
+import { User } from '@prisma/client';
+import { hash } from 'bcrypt';
+import { UsersService } from '../users/users.service';
 
 describe('The AuthenticationService', () => {
+  let userData: User;
   let authenticationService: AuthenticationService;
+  let password: string;
   beforeEach(async () => {
+    password = 'strongPassword123';
+    const hashedPassword = await hash(password, 10);
+    userData = {
+      id: 1,
+      email: 'john@smith.com',
+      name: 'John',
+      password: hashedPassword,
+      addressId: null,
+      phoneNumber: null,
+    };
     const module = await Test.createTestingModule({
-      providers: [AuthenticationService],
+      providers: [
+        AuthenticationService,
+        {
+          provide: UsersService,
+          useValue: {
+            getByEmail: jest.fn().mockResolvedValue(userData),
+          },
+        },
+      ],
       imports: [
-        UsersModule,
         ConfigModule.forRoot(),
         JwtModule.register({
           secretOrPrivateKey: 'Secret key',
         }),
-        DatabaseModule,
       ],
     }).compile();
 
@@ -26,6 +45,17 @@ describe('The AuthenticationService', () => {
     it('should return a correct string', () => {
       const result = authenticationService.getCookieForLogOut();
       expect(result).toBe('Authentication=; HttpOnly; Path=/; Max-Age=0');
+    });
+  });
+  describe('when the getAuthenticatedUser method is called', () => {
+    describe('and a valid email and password are provided', () => {
+      it('should return the new user', async () => {
+        const result = await authenticationService.getAuthenticatedUser({
+          email: userData.email,
+          password,
+        });
+        expect(result).toBe(userData);
+      });
     });
   });
 });
