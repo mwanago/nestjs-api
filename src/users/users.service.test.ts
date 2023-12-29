d92686a0e49de5d016c9d1f2c8c536332f6237a8
Marcin <wanago.marcin@gmail.com>
2023-12-29 04:23:46 +0100

    test(Users): test the create method

--- src/users/users.service.test.ts
+++ src/users/users.service.test.ts
@@ -1,14 +1,18 @@
 import { UsersService } from './users.service';
-import { User } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 import { Test } from '@nestjs/testing';
 import { PrismaService } from '../database/prisma.service';
-import { NotFoundException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { UserDto } from './user.dto';
+import { PrismaError } from '../database/prisma-error.enum';
 
 describe('The UsersService', () => {
   let usersService: UsersService;
   let findUniqueMock: jest.Mock;
+  let createMock: jest.Mock;
   beforeEach(async () => {
     findUniqueMock = jest.fn();
+    createMock = jest.fn();
     const module = await Test.createTestingModule({
       providers: [
         UsersService,
@@ -17,6 +21,7 @@ describe('The UsersService', () => {
           useValue: {
             user: {
               findUnique: findUniqueMock,
+              create: createMock,
             },
           },
         },
@@ -52,6 +57,49 @@ describe('The UsersService', () => {
         return expect(async () => {
           await usersService.getById(1);
         }).rejects.toThrow(NotFoundException);
+      });
+    });
+  });
+  describe('when the create function is called with valid data', () => {
+    let userData: UserDto;
+    beforeEach(() => {
+      userData = {
+        email: 'john@smith.com',
+        name: 'John',
+        password: 'strongPassword123',
+      };
+    });
+    describe('and the prismaService.create returns a valid user', () => {
+      let user: User;
+      beforeEach(() => {
+        user = {
+          email: 'john@smith.com',
+          name: 'John',
+          password: 'strongPassword123',
+          id: 1,
+          addressId: null,
+          phoneNumber: null,
+        };
+        createMock.mockResolvedValue(user);
+      });
+      it('should return the created user', async () => {
+        const result = await usersService.create(userData);
+        expect(result).toBe(user);
+      });
+    });
+    describe('and the prisma.create causes the UniqueConstraintViolated error', () => {
+      beforeEach(() => {
+        createMock.mockRejectedValue(
+          new Prisma.PrismaClientKnownRequestError('Unique constraint failed', {
+            code: PrismaError.UniqueConstraintViolated,
+            clientVersion: Prisma.prismaVersion.client,
+          }),
+        );
+      });
+      it('should throw the ConflictException error', () => {
+        return expect(async () => {
+          await usersService.create(userData);
+        }).rejects.toThrow(ConflictException);
       });
     });
   });
