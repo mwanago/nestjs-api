b5e97adb37f7279fd43097ae9f1fe0bb75e5bc17
Marcin <wanago.marcin@gmail.com>
2024-07-10 02:10:40 +0200

    test(Authentication): test if the AuthenticationService calls the PrismaService with the correct data

--- src/authentication/authentication.service.integration.test.ts
+++ src/authentication/authentication.service.integration.test.ts
@@ -6,6 +6,12 @@ import { UsersService } from '../users/users.service';
 import { PrismaService } from '../database/prisma.service';
 import { SignUpDto } from './dto/sign-up.dto';
 
+jest.mock('bcrypt', () => ({
+  hash: () => {
+    return Promise.resolve('hashed-password');
+  },
+}));
+
 describe('The AuthenticationService', () => {
   let authenticationService: AuthenticationService;
   let prismaCreateMock: jest.Mock;
@@ -16,7 +22,8 @@ describe('The AuthenticationService', () => {
       email: 'john@smith.com',
       name: 'John',
       password: 'strongPassword123',
-    }
+      phoneNumber: '123456789',
+    };
     const module = await Test.createTestingModule({
       providers: [
         AuthenticationService,
@@ -43,7 +50,18 @@ describe('The AuthenticationService', () => {
   describe('when the signUp function is called', () => {
     it('should call the create method from the PrismaService', async () => {
       await authenticationService.signUp(signUpData);
-      expect(prismaCreateMock).toBeCalled();
-    })
-  })
+      expect(prismaCreateMock).toBeCalledWith({
+        data: {
+          ...signUpData,
+          address: {
+            create: signUpData.address,
+          },
+          password: 'hashed-password',
+        },
+        include: {
+          address: true,
+        },
+      });
+    });
+  });
 });
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
 import { SignUpDto } from './dto/sign-up.dto';
